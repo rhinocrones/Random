a3c6d14fc44c8f6b3650ad0cd496ca34b85140b9
Rinox <Rinox@Rinox-PC>
2016-08-27 22:30:10 +0300

    Second commit

--- Random/src/ua/Main.java
+++ Random/src/ua/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Random");
-
+		System.out.println("Random two");
+		System.out.println("Random three");
 	}
 
 }
